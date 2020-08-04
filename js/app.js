9ec2062e4eab113810b50a4dc7a869092454aeff
LeeThomas13 <lmtthomasritz@gmail.com>
2020-08-04 16:06:22 -0700

    added first function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 'use strict';
 
-var 
+var userName = prompt('Hey there :) what\'s your name? ')
+console.log ('users name is: ' + userName)
+alert( 'Hey ' + userName + ' my name is Lee, welcome to my about me website')
+
