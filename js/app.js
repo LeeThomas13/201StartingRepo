a581c1927282426c452cef470c82083c7a7378ee
LeeThomas13 <lmtthomasritz@gmail.com>
2020-08-05 09:30:02 -0700

    fixed all javascript to correctly reflect the assignment

--- js/app.js
+++ js/app.js
@@ -4,65 +4,64 @@ var userName = prompt('Hey there :) what\'s your name? ')
 console.log ('users name is: ' + userName)
 alert( 'Hey ' + userName + ' my name is Lee, welcome to my about me website')
 
-var food = prompt('So ' + userName + ' do you like ramen?');
+var game = prompt('So ' + userName + ' do you want to play a guessing game?');
 
-console.log ('does the user like ramen: ' + food)
+console.log ('does the user want to play a guessing game: ' + game)
 
-if(food.toLowerCase() === 'yes' || food === 'y'){
-alert('Cool\! Ramen is my favorite food.');
+if(game.toLowerCase() === 'yes' || game === 'y'){
+alert('Cool\! Lets get started.');
 
-}else if(food.toLowerCase() === 'no' || food === 'n'){
-alert('bummer\, Ramen is my favorite food.');
+}else if(game.toLowerCase() === 'no' || game === 'n'){
+alert('bummer\, well you\'re on my website so you dont have a choice >:(');
 
 }else {
 alert('please enter yes or no');
 
 }
 
-var snowboardPreference = prompt( 'Well ' + userName + ' do you like to snowboard?')
-console.log ('does the user like to snowboard? ' + snowboardPreference)
-
-if(snowboardPreference.toLowerCase() === 'yes' || snowboardPreference === 'y'){
-alert('Wow! snowboarding is my favorite outdoors activity!')
-}else if(snowboardPreference.toLowerCase() === 'no' || snowboardPreference === 'n'){
-alert('Aww its so fun though :\(')
+var bornWashington = prompt( 'Well ' + userName + ' was I born in Washington D.C.?')
+console.log ('was i born in washington d.c.? ' + bornWashington )
+if(bornWashington.toLowerCase() === 'yes' || bornWashington === 'y'){
+alert('Your are correct! good job.')
+}else if(bornWashington.toLowerCase() === 'no' || bornWashington === 'n'){
+alert('ERRR, nice try but I was!')
 }else {
 alert('say yes or no next time please')
 }
 
-var state = prompt('Do you live in Washington ' + userName + '?')
-console.log('does the user live in washington: ' + state)
+var food = prompt('Is my favorite food sushi?')
+console.log('is my favorite food pizza: ' + food)
 
-if(state.toLowerCase() === 'yes' || state === 'y'){
-alert('Very cool ' + userName + '!' + ' I plan on moving there as soon as covid calms down.');
-}else if(state.toLowerCase() === 'no' || state === 'n'){
-alert('oh ok, either way I plan on moving there once covid calms down.');
+if(food.toLowerCase() === 'yes' || food === 'y'){
+alert('ERRR, close! But my favorite food is ramen. Sushi is a close second.');
+}else if(food.toLowerCase() === 'no' || food === 'n'){
+alert('Correct! My favorite food is ramen.');
 }else{
   alert('ok ' + userName + ',' + ' you gotta answer with a yes or no');
 }
 
-var height = prompt('so ' + userName + ' are you over 6 feet tall?');
-console.log('is the user over six feet tall: ' + height)
+var barista = prompt('Have I ever worked at Starbucks as a barista?');
+console.log('have i worked as a barista: ' + barista)
 
-if(height.toLowerCase() === 'yes' || height === 'y'){
-  alert('hey me too!')
-}else if(height.toLowerCase() === 'no' || height === 'n'){
-  alert('you\'re lucky, the lower back problems are real');
+if(barista.toLowerCase() === 'yes' || barista === 'y'){
+  alert('ERRR, I have not worked as a barista, but my brother did!')
+}else if(barista.toLowerCase() === 'no' || barista === 'n'){
+  alert('Correct! I\'ve never worked as a barista, but I have worked at a flower shop!');
 }else{
   alert('OK ' + userName + ',' + ' ITS TIME TO ANSWER WITH A YES OR A NO');
 }
 
-var smash = prompt('Ok ' + userName + ',' + ' last question, have you ever played Super Smash Bros?')
-console.log('has the user ever played ssb: ' + smash);
+var smash = prompt('Is Super Smash Bro\'s my favorite game?')
+console.log('is ssb my fav game: ' + smash);
 
 if(smash.toLowerCase() === 'yes' || smash === 'y'){
-  alert('Thats so cool! You cant beat my ness though :)');
+  alert('Correct! I used to compete at locals with Ness');
 }else if(smash.toLowerCase() === 'no' || smash === 'n'){
-  alert('You have to try it! It\'s my favorite game of all time!');
+  alert('ERRR, it is my favorite game :)');
 }else{
   alert('i give up');
 }
 
-alert('Well thats it for the interrogation, here\'s a little website about me')
+alert('Well thats it for the guessing game, here\'s a little website about me')
 
 
