bc2a81a3f3592ec7ff5caf283488995512ec5794
LeeThomas13 <lmtthomasritz@gmail.com>
2020-08-10 16:08:04 -0700

    finished the webpage

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 'use strict';
+var finalCorrectScore = 0
+
+var finalWrongScore = 0
 
 var userName = prompt('Hey there :) what\'s your name? ')
-  console.log ('users name is: ' + userName)
   alert( 'Hey ' + userName + ' my name is Lee, welcome to my about me website')
 
 function Website(){
 
   var game = prompt('So ' + userName + ' do you want to play a guessing game?');
-
-  console.log ('does the user want to play a guessing game: ' + game)
 
   if(game.toLowerCase() === 'yes' || game.toLowerCase() === 'y'){
   alert('Cool\! Lets get started.');
@@ -24,10 +24,13 @@ Website();
 
 function Loveland(){
   var bornWashington = prompt( 'Well ' + userName + ' have I lived in a city called Loveland?')
-  console.log ('have i lived in Loveland ' + bornWashington )
+
   if(bornWashington.toLowerCase() === 'yes' || bornWashington.toLowerCase() === 'y'){
+    finalCorrectScore++;
+    finalWrongScore++;
   alert('Your are correct! good job.')
   }else if(bornWashington.toLowerCase() === 'no' || bornWashington.toLowerCase() === 'n'){
+    finalWrongScore++;
   alert('ERRR, nice try but I was!')
   }else {
   alert('say yes or no next time please')
@@ -38,12 +41,15 @@ Loveland();
 
 function sushi(){
   var food = prompt('Is my favorite food sushi?')
-  console.log('is my favorite food pizza: ' + food)
 
   if(food.toLowerCase() === 'yes' || food.toLowerCase() === 'y'){
+    finalWrongScore++;
   alert('ERRR, close! But my favorite food is ramen. Sushi is a close second.');
   }else if(food.toLowerCase() === 'no' || food.toLowerCase() === 'n'){
+    finalCorrectScore++;
+    finalWrongScore++;
   alert('Correct! My favorite food is ramen.');
+
   }else{
     alert('ok ' + userName + ',' + ' you gotta answer with a yes or no');
   }
@@ -53,11 +59,13 @@ sushi();
 
 function flower(){
   var barista = prompt('Have I ever worked at Starbucks as a barista?');
-  console.log('have i worked as a barista: ' + barista)
 
   if(barista.toLowerCase() === 'yes' || barista.toLowerCase() === 'y'){
+    finalWrongScore++;
     alert('ERRR, I have not worked as a barista, but my brother did!')
   }else if(barista.toLowerCase() === 'no' || barista.toLowerCase() === 'n'){
+    finalCorrectScore++;
+    finalWrongScore++;
     alert('Correct! I\'ve never worked as a barista, but I have worked at a flower shop!');
   }else{
     alert('OK ' + userName + ',' + ' ITS TIME TO ANSWER WITH A YES OR A NO');
@@ -69,11 +77,13 @@ flower();
 function Ness(){
 
   var smash = prompt('Is Super Smash Bro\'s my favorite game?')
-  console.log('is ssb my fav game: ' + smash);
 
   if(smash.toLowerCase() === 'yes' || smash.toLowerCase() === 'y'){
+    finalCorrectScore++;
+    finalWrongScore++;
     alert('Correct!');
   }else if(smash.toLowerCase() === 'no' || smash.toLowerCase() === 'n'){
+    finalWrongScore++;
     alert('ERRR, it is my favorite game :)');
   }else{
     alert('i give up');
@@ -92,11 +102,15 @@ function numGuess(){
     var myNum = prompt('I am thinking of a number between 1-20. You have 4 tries at guessing it. Good Luck!');
     if(Number(myNum) < numAnswer){
       numTries++;
+      finalWrongScore++;
       alert('Too low!');
     } else if (parseInt(myNum) > numAnswer){
       numTries++;
+      finalWrongScore++;
       alert('Too high!');
     } else if (parseInt(myNum) === numAnswer){
+      finalCorrectScore++;
+      finalWrongScore++;
       alert('Good Job! You guessed correctly in ' + numTries + ' attempts!');
       break;
     }
@@ -110,7 +124,7 @@ function numGuess(){
 numGuess();
 
 function Subaru(){
-  var carAnswer = ['Volkswagen','Maserati','Subaru','Ford'];
+  var carAnswer = ['Volkswagen','Maserati','Subaru','Ford', 'Audi'];
 
   var carTries = 1
 
@@ -120,16 +134,19 @@ function Subaru(){
     for(var x=0;x<carAnswer.length;x++){
       if(carAnswer[x].toLowerCase() === carPing.toLowerCase()){
         alert('Correct!');
-        carCorrect = true
+        finalCorrectScore++;
+        finalWrongScore++;
+        carCorrect = true;
       }
     }
     if(carCorrect === true){
       break;
     }else{
+      finalWrongScore++;
       alert ('sorry try again');
     }
     if(i === 6){
-      alert('sorry you are out of guesses :(');
+      alert('sorry you are out of guesses :( the answers are Volkswagen, Maserati, Subaru, Ford, or Audi');
       break;
     }
   }
@@ -137,6 +154,6 @@ function Subaru(){
 
 Subaru();
 
-alert('Ok ' + userName + ' thats all the questions I have for you!')
+alert('Ok ' + userName + ' thats all six questions I have for you! Looks like you got ' + finalCorrectScore + ' correct out of ' + finalWrongScore + ' attempts.')
 
 
